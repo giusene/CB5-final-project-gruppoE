0e7fa950b88e5e2e12306eb39c38e80aff84fad2
marcorivera92 <marco.rivera92@gmail.com>
2023-02-15 22:18:38 +0100

    feat/login-page

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,13 +1,16 @@
 import styles from "./styles.module.scss";
 import Header from "@/components/header/Header";
 import Navbar from "@/components/navbar/Navbar";
+import Login from "@/pages/login";
 
 const MainLayout = ({ children }) => {
   return (
     <div className={styles.main}>
+      {/* <Login> */}
       <Header />
       <Navbar />
       {children}
+      {/* </Login> */}
     </div>
   );
 };
