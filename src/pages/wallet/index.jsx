f1ec85c90a06295b325bd577afb3ea43b388533c
Sebastiano Occhipinti <sebastianoocchipinti25@gmail.com>
2023-02-16 12:18:13 +0100

    feat: graphic with chart.js,wallet page

--- src/pages/wallet/index.jsx
+++ src/pages/wallet/index.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import LineChart from "@/components/chart/LineChart";
+import styles from "@/components/chart/styles.module.scss";
 
-const Wallet = () => {
-  return <div>wallet</div>;
-};
+function Wallet() {
+  const data = {
+    labels: [
+      "Gennaio",
+      "Febbraio",
+      "Marzo",
+      "Aprile",
+      "Maggio",
+      "Giugno",
+      "Luglio",
+      "Agosto",
+      "Settembre",
+      "Ottobre",
+      "Novembre",
+      "Dicembre",
+    ],
+    datasets: [
+      {
+        label: "Saldo",
+        data: [
+          1000, 1200, 1300, 1500, 1800, 2000, 2200, 2400, 2600, 2800, 3000,
+          3200,
+        ],
+        borderColor: "purple",
+        fill: false,
+      },
+    ],
+  };
+  const options = {
+    plugins: {
+      title: {
+        display: true,
+        text: "Chart.js Stacked Line/Bar Chart",
+      },
+    },
+
+    scales: {
+      y: {
+        ticks: {
+          stacke: true,
+        },
+      },
+    },
+  };
+
+  return (
+    <div className={styles.main}>
+      <h1>Il mio Saldo</h1>
+      <div className={styles.chart}>
+        <LineChart data={data} options={options} />
+      </div>
+    </div>
+  );
+}
 
 export default Wallet;
