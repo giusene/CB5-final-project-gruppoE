40a9927557c1e9657e64999204776362b883f1c3
marcorivera92 <marco.rivera92@gmail.com>
2023-02-16 20:08:58 +0100

    feat: trade cart and page style

--- src/components/coinsitem/CoinsItem.jsx
+++ src/components/coinsitem/CoinsItem.jsx
@@ -1,8 +1,11 @@
-import styles from "./styles.module.scss";
+import { currencyFormat } from "@/utils/currencyFormat";
 import { AiOutlineStar } from "react-icons/ai";
+import { FiTrendingUp, FiTrendingDown } from "react-icons/fi";
+import styles from "./styles.module.scss";
 
 const CoinsItem = ({ data }) => {
-  const { image, name, symbol, market_cap_rank } = data;
+  console.log(data);
+  const { image, name, symbol, current_price, price_change_percentage_24h } = data;
 
   return (
     <div className={styles.main}>
@@ -13,14 +16,27 @@ const CoinsItem = ({ data }) => {
           <p className={styles.symbol}>{symbol}</p>
         </div>
       </div>
+      <div className={styles.price}>
+        <div>
+          <p>{currencyFormat(current_price)}</p>
+        </div>
+        <div>
+          <p
+            className={`${styles.price_change} ${
+              price_change_percentage_24h < 0 ? styles.price_down : styles.price_up
+            }`}
+          >
+            {price_change_percentage_24h < 0 ? <FiTrendingDown /> : <FiTrendingUp />}
+            <span> </span>
+            {price_change_percentage_24h}
+          </p>
+        </div>
+      </div>
       <div className={styles.button_wrapper}>
         <button>BUY</button>
-        <div className={styles.price}>
-          <i>
-            <AiOutlineStar />
-          </i>
-          <p>{market_cap_rank} USD</p>
-        </div>
+        <i>
+          <AiOutlineStar />
+        </i>
       </div>
     </div>
   );
