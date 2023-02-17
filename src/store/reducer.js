d73f42579ade7964346bb0e402cf2ae570c90285
magic_c <info.chiaramaggio@gmail.com>
2023-02-17 10:23:34 +0100

    edit-reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,37 +1,40 @@
-import { loginActions } from "./actions";
+import { loginActions, cartActions } from "./actions";
 
 const appReducer = (state, action) => {
-  if (action.type === loginActions.LOGIN_USER) {
-    const user = state.users.find(
-      (user) =>
-        user.username === action.payload.username && user.password === action.payload.password
-    );
-    if (user) {
-      localStorage.setItem("currentUser", JSON.stringify(user));
+  switch (action.type) {
+    case loginActions.LOGIN_USER:
+      const user = state.users.find(
+        (user) =>
+          user.username === action.payload.username &&
+          user.password === action.payload.password
+      );
+      if (user) {
+        localStorage.setItem("currentUser", JSON.stringify(user));
+        return {
+          ...state,
+          currentUser: user,
+          currentBalance: user.balance || 0,
+          currentCreditCard: user.creditCard,
+          cart: user.cart || [],
+          isLogged: true,
+        };
+      } else {
+        return state;
+      }
+
+    case loginActions.LOGOUT_USER:
+      localStorage.removeItem("currentUser");
       return {
         ...state,
-        currentUser: user,
-        currentBalance: user.balance || 0,
-        currentCreditCard: user.creditCard,
-        cart: user.cart || [],
-        isLogged: true,
+        currentUser: "",
+        currentBalance: 0,
+        currentCreditCard: null,
+        cart: [],
+        isLogged: false,
       };
-    } else {
-      console.log("username o password non validi");
-      return state;
-    }
-  }
 
-  if (action.type === loginActions.LOGOUT_USER) {
-    localStorage.removeItem("currentUser");
-    return {
-      ...state,
-      currentUser: "",
-      currentBalance: 0,
-      currentCreditCard: null,
-      cart: [],
-      isLogged: false,
-    };
+    default:
+      return state;
   }
 };
 
