f69274d7b2f01f9d0c45d4efff0c08dcd397fadc
pietrodistefano23 <pietrodistefano2306@gmail.com>
2023-02-17 15:10:44 +0100

    Feat/nft-modal

--- src/components/nftList/NftList.jsx
+++ src/components/nftList/NftList.jsx
@@ -1,15 +1,51 @@
+import { useState } from "react";
 import NftItem from "../nftItem/NftItem";
+import Modal from "../modal/Modal";
 import styles from "../nftList/styles.module.scss";
 
 const NftList = ({ data }) => {
+  const [modalState, setModalState] = useState(false);
+
+  let modalObj = {
+    name:'',
+    description: '',
+    rarity: ''
+
+  }
+  const [modalCtx, setModalCtx] = useState(modalObj)
+
+ 
+  const HandleModal = (value,img) =>{
+    console.log(img)
+    setModalState(() => !modalState);
+    setModalCtx(modalObj= {
+      name: value.contract.name,
+      description: value.contract.metadata.description,
+      image: img
+     
+      
+    });
+    
+    
+     
+    }
+  
+  
   // data = nfts + contract
-  console.log(data);
 
   return (
     <div className={styles.main}>
-      {data.nfts.map((item, index) => (
-        <NftItem data={item} key={index} contract={data.contract} />
-      ))}
+      <div className={styles.list}>
+          {data.nfts.map((item, index) => (
+            <NftItem item={item} key={index} data={data} HandleModal={HandleModal} />
+          ))}
+      </div>
+      
+        { modalState && <Modal data={modalCtx}/> }
+      
+      
+      
+      
     </div>
   );
 };
