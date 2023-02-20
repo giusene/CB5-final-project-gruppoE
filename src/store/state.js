4831581bcf574f5ab379782564e2c816d82252b5
magic_c <info.chiaramaggio@gmail.com>
2023-02-20 12:58:11 +0100

    fix-strutturadati

--- src/store/state.js
+++ src/store/state.js
@@ -3,10 +3,6 @@ import users from "./users";
 const initialState = {
   users: users,
   currentUser: "",
-  currentBalance: 0,
-  currentCreditCard: null,
-  cart: [],
-  favorite: [],
   isLogged: false,
 };
 
