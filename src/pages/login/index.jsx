c7af7b1887643c7deebdf4adb38b6b40c9ce699c
marcorivera92 <marco.rivera92@gmail.com>
2023-02-24 18:03:55 +0100

    feat: logo animation

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,11 +1,11 @@
 import AuthCard from "@/components/authCard/AuthCard";
 import styles from "@/styles/login.module.scss";
 import Image from "next/image";
-import CryptoLogo from "../../../public/logo/crypto-logo.svg";
+import CryptoLogoAnim from "../../../public/logo/crypto-logo-anim.svg";
 const Login = () => {
   return (
     <div className={styles.main}>
-      <Image src={CryptoLogo} alt="logo" width={60} height={60} />
+      <Image src={CryptoLogoAnim} alt="logo" width={60} height={60} />
       <AuthCard />
     </div>
   );
