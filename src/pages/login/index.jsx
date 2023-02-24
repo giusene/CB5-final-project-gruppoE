93c85a5556a5196a551a2aa104b90ea33fec7f1c
magic_c <info.chiaramaggio@gmail.com>
2023-02-24 11:25:51 +0100

    css-login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,11 @@
 import AuthCard from "@/components/authCard/AuthCard";
 import styles from "@/styles/login.module.scss";
-
+import Image from "next/image";
+import CryptoLogo from "../../../public/logo/crypto-logo.svg";
 const Login = () => {
   return (
     <div className={styles.main}>
+      <Image src={CryptoLogo} alt="logo" width={60} height={60} />
       <AuthCard />
     </div>
   );
